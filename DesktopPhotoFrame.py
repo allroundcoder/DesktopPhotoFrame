f9169a1dde964d4a30fce9aab9f03e1815b37db7
allroundcoder <allroundcoder@gmail.com>
2018-05-25 14:43:29 +0200

    - Add triple mouse click as quit command - Use percentage of screen height instead of screen width - Moved general settings to the top of the file

--- DesktopPhotoFrame.py
+++ DesktopPhotoFrame.py
@@ -11,38 +11,51 @@ if sys.version_info < (3, 0):
 else:
     import tkinter as tk
 
+###################
+# General setings #  
+###################
+SLIDE_SHOW_TIME = 300
+
+# Window height in landscape orientation. The same value is used
+# for the window width in landscape orientation.
+WINDOW_HEIGHT_IN_PERCENTAGE_OF_SCREEN_HEIGHT = 30
+###################
+
 class Win(tk.Tk):
     def __init__(self,master=None):
         tk.Tk.__init__(self,master)
         self.overrideredirect(True)
         self.wm_attributes("-topmost", 1)
-        self._offsetx = 0
-        self._offsety = 0
+        self.offsetx = 0
+        self.offsety = 0
         self.bind('<ButtonPress-1>',self.clickwin)
         self.bind('<B1-Motion>',self.dragwin)
-        
+
     def dragwin(self,event):
-        x = self.winfo_pointerx() - self._offsetx
-        y = self.winfo_pointery() - self._offsety
+        x = self.winfo_pointerx() - self.offsetx
+        y = self.winfo_pointery() - self.offsety
         self.geometry('+{x}+{y}'.format(x=x,y=y))
 
     def clickwin(self,event):
-        self._offsetx = event.x
-        self._offsety = event.y
-    
+        self.offsetx = event.x
+        self.offsety = event.y
+
 class App():
     def __init__(self):
         self.root = Win()
         self.root.pack_propagate(False)
         self.root.config(bg="black", width=1, height=1)
-        self._images = [img for img in os.listdir('.') if img.lower()[-4:] in ('.jpg', '.png', '.gif')]
-        self._image_pos = -1
-
-        self.root.bind("<space>", self.space_handler)
+        self.images = [img for img in os.listdir('.') if img.lower()[-4:] in ('.jpg', '.png', '.gif')]
+        self.image_pos = -1
+        self.last_view_time = 0
+        self.image = None
+    
         self.root.bind("<Escape>", self.esc_handler)
         self.root.bind("<Left>", self.show_previous_image)
         self.root.bind("<Right>", self.show_next_image)
         self.root.bind("q", self.esc_handler)
+        self.root.bind('<Triple-Button-1>',self.esc_handler)
+        
         self.root.after(100, self.show_next_image)
 
         self.label = tk.Label(self.root, image=None)
@@ -50,45 +63,34 @@ class App():
         self.label.pack()
 
         self.set_timer()
+        self.root.focus_set()
         self.root.mainloop()
     
-    # setings   
-    slide_show_time = 300
-    last_view_time = 0
-    paused = False
-    image = None
-
-    # Window width in landscape orientation. The same value is used
-    # for the window height in portrait orientation.
-    window_width_in_percentage_of_screen_width = 20
-
     def esc_handler(self, e):
         self.root.destroy()
-
-    def space_handler(self, _):
-        self.paused = not self.paused
 
     def set_timer(self):
         self.root.after(300, self.update_clock)
 
     def update_clock(self):
-        if time.time() - self.last_view_time > self.slide_show_time \
-           and not self.paused:
+        if time.time() - self.last_view_time > SLIDE_SHOW_TIME:
             self.show_next_image()
         self.set_timer()
 
     def next_image(self):
-        if not self._images: 
+        if not self.images: 
             return None
-        self._image_pos += 1
-        self._image_pos %= len(self._images)
-        return self._images[self._image_pos]
+        self.image_pos += 1
+        self.image_pos %= len(self.images)
+        return self.images[self.image_pos]
 
     def previous_image(self):
-        if not self._images: 
+        if not self.images: 
             return None
-        self._image_pos -= 1
-        return self._images[self._image_pos]
+        
+        if self.image_pos > 0:
+            self.image_pos -= 1
+        return self.images[self.image_pos]
 
     def show_next_image(self, e=None):
         fname = self.next_image()
@@ -125,16 +127,16 @@ class App():
         win_center_x = win_x + (win_width / 2)
         win_center_y = win_y + (win_height / 2)
 
-        new_win_width_land = int(screen_width * (self.window_width_in_percentage_of_screen_width / 100.0))
+        new_win_height_port = int(screen_height * (WINDOW_HEIGHT_IN_PERCENTAGE_OF_SCREEN_HEIGHT / 100.0))
         
         # size
         if width > height:
             # landscape
-            new_win_width = new_win_width_land
-            new_win_height = int(new_win_width / aspect_ratio)  
+            new_win_width = new_win_height_port
+            new_win_height = int(new_win_width / aspect_ratio)
         else:
             # portrait
-            new_win_height = new_win_width_land
+            new_win_height = new_win_height_port
             new_win_width = int(new_win_height * aspect_ratio)
             
         new_size = (new_win_width,new_win_height)
@@ -155,7 +157,7 @@ class App():
             new_y = win_y + win_height - new_win_height
             
         # apply changes
-        self.root.geometry('{}x{}+{}+{}'.format(new_win_width, new_win_height, new_x, new_y))
+        self.root.geometry('{0}x{1}+{2}+{3}'.format(new_win_width, new_win_height, new_x, new_y))
         self.image = self.original_image.resize(new_size, Image.ANTIALIAS)
         self.label.place(x=0, y=0, width=new_win_width,height=new_win_height)
         tkimage = ImageTk.PhotoImage(self.image)
